c6d9621068efe0056f47967f9ba40013dd788383
Rafael Arraez <rafa.arraez.gue@gmail.com>
2021-01-06 22:03:15 -0400

    ultimos cambios

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDXWwFVHNqlSZkxQburYQGChcG-QXTX-HQ",
+    authDomain: "test-heyandes-rafael-arraez.firebaseapp.com",
+    projectId: "test-heyandes-rafael-arraez",
+    storageBucket: "test-heyandes-rafael-arraez.appspot.com",
+    messagingSenderId: "534332387363",
+    appId: "1:534332387363:web:20d6b576c91a7ccbce4196",
+    measurementId: "G-J3LEDXNW9X"
+  }
 };
